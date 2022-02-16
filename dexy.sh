9ed88ec98a0f07308bcf56f20bfff5ac39168b77
karlkoorna <karlkoorna@gmail.com>
2022-02-16 22:05:54 +0200

    Update

--- dexy.sh
+++ dexy.sh
@@ -30,21 +30,21 @@ if [[ ! $APK_PATH ]]; then
 	exit 1
 fi
 
-rm -rf tmp
-
-echo "Pulling app from device..."
-adb pull $APK_PATH tmp/app.apk &> /dev/null
-
-if [[ $? -ne 0 ]]; then
-	echo "Failed to pull!"
-	exit 1
-fi
+rm -r tmp/
 
 echo "Downloading apktool..."
 curl -s -S -L -o tmp/apktool.jar https://github.com/iBotPeaches/Apktool/releases/download/v2.6.0/apktool_2.6.0.jar
 
 if [[ $? -ne 0 ]]; then
 	echo "Failed to download!"
+	exit 1
+fi
+
+echo "Pulling app from device..."
+adb pull $APK_PATH tmp/app.apk &> /dev/null
+
+if [[ $? -ne 0 ]]; then
+	echo "Failed to pull!"
 	exit 1
 fi
 
@@ -57,9 +57,9 @@ if [[ $? -ne 0 ]]; then
 fi
 
 echo "Patching app..."
-sed -i -E 's#(<application.+)>#\1 android:networkSecurityConfig="@xml/network_security_config">#' tmp/app/AndroidManifest.xml &&
 sed -i 's#.end param#.end param\ninvoke-virtual {p0}, Lcom/dexcom/dexcomone/ui/acm/AcmActivity;->finish()V#' tmp/app/smali_classes2/com/dexcom/dexcomone/ui/acm/AcmActivity.smali
-cp network_security_config.xml tmp/app/res/xml/
+# sed -i -E 's#(<application.+)>#\1 android:networkSecurityConfig="@xml/network_security_config">#' tmp/app/AndroidManifest.xml &&
+# cp network_security_config.xml tmp/app/res/xml/
 
 if [[ $? -ne 0 ]]; then
 	echo "Failed to patch!"
@@ -75,7 +75,7 @@ if [[ $? -ne 0 ]]; then
 fi
 
 echo "Signing app..."
-keytool -genkey -keystore tmp/keystore -storepass password -keyalg EC -groupname secp384r1 -alias Dexy -dname "CN=Dexy, OU=Dexy, O=Dexy, L=Dexy, S=Dexy, C=XX" &&
+keytool -genkey -keystore tmp/keystore -storepass password -keyalg EC -groupname secp384r1 -alias Dexy -dname "CN=Dexy, OU=Dexy, O=Dexy, L=Dexy, S=Dexy, C=XX" > /dev/null &&
 jarsigner -keystore tmp/keystore -storepass password -sigalg SHA256withECDSA -digestalg SHA-256 tmp/app-patched.apk Dexy > /dev/null
 
 if [[ $? -ne 0 ]]; then
@@ -89,4 +89,5 @@ adb uninstall com.dexcom.dexcomone > /dev/null &&
 adb install tmp/app-patched.apk &> /dev/null
 adb shell am start -n com.dexcom.dexcomone/com.dexcom.phoenix.ui.SplashActivity > /dev/null
 
+rm -r tmp/
 echo "Done"
